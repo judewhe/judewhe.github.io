6e05c93011647b269f65a3dd3f619d32e6d61f34
judewhe <judewhlr@gmail.com>
2026-07-13 17:31:58 +0100

    hmmm

--- main.js
+++ main.js
@@ -6,6 +6,4 @@ function sleep(time){
 function AnimateButton(){
     console.log("BIG RED BUTTON PRESSED")
     document.getElementById("circle").classList.add("animate");
-    sleep(0.1);
-    document.getElementById("circle").classList.remove("animate");
 }
