03ca5f108d7083845cc2297275ff64af69a5d9d9
judewhe <judewhlr@gmail.com>
2026-07-14 18:59:13 +0100

    animate fix possibly

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-function sleep(time){
-    return new Promise(resolve => setTimeout(resolve,time));
-}
-
-
 function AnimateButton(){
     console.log("BIG RED BUTTON PRESSED")
     document.getElementById("circle").classList.add("animate");
+    setTimeout(RemoveGrowAnimation,100)
+}
+function RemoveGrowAnimation(){
+    document.getElementById("circle").classList.remove("animate")
 }
