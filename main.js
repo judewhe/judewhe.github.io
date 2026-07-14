3b21c88957333dbd10c5e23e37539d987ceb3f38
judewhe <judewhlr@gmail.com>
2026-07-14 20:07:08 +0100

    yh

--- main.js
+++ main.js
@@ -2,15 +2,11 @@ var score = 0;
 
 function AnimateButton(){
     document.getElementById("circle").classList.add("animategrow");
-    setTimeout(RemoveGrowAnimation,IncreaseScore,100);
+    score = score + 1;
+    document.getElementById("score").textContent="Score" + String.valueOf(score)
+    setTimeout(RemoveGrowAnimation,100);
 
 }
 function RemoveGrowAnimation(){
     document.getElementById("circle").classList.remove("animategrow");
-    IncreaseScore()
-}
-
-function IncreaseScore(){
-    score = score + 1;
-    document.getElementById("score").textContent="Score" + String.valueOf(score)
 }
