c3f6d33df5962ca3cc8e8a474a39df784a0acb8f
judewhe <judewhlr@gmail.com>
2026-07-14 20:01:38 +0100

    score

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+var score = 0;
+
 function AnimateButton(){
     document.getElementById("circle").classList.add("animategrow");
+    IncreaseScore()
     setTimeout(RemoveGrowAnimation,100);
 }
 function RemoveGrowAnimation(){
     document.getElementById("circle").classList.remove("animategrow");
+}
+
+function IncreaseScore(){
+    score = score + 1;
+    document.getElementById("score").textContent="Score" + String.valueOf(score)
 }
