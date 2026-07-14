14a80caae56e4b2ff19ff6412b0a3117b5376ce6
judewhe <judewhlr@gmail.com>
2026-07-14 19:14:50 +0100

    changing animation

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 function AnimateButton(){
     document.getElementById("circle").classList.add("animategrow");
-    setTimeout(RemoveGrowAnimation,50);
+    setTimeout(RemoveGrowAnimation,100);
 }
 function RemoveGrowAnimation(){
     document.getElementById("circle").classList.remove("animategrow");
-    document.getElementById("circle").classList.add("animateshrink");
-    setTimeout(RemoveShrinkAnimatio,50);
-}
-function RemoveShrinkAnimatio(){
-    document.getElementById("circle").classList.remove("animateshrink");
 }
