bd7f0208d65e6a8a83f162be9bd0174385aa2027
judewhe <judewhlr@gmail.com>
2026-07-15 09:53:43 +0100

    Add score multiplier to score calculation

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 var score = 0;
+var score_mult = 1
 
 function AnimateButton(){
     document.getElementById("circle").classList.add("animategrow");
-    score = score + 1;
+    score = score + (1 * score_mult);
     document.getElementById("score").textContent="Score: "+String(score);
     setTimeout(RemoveGrowAnimation,100);
 
 }
 function RemoveGrowAnimation(){
     document.getElementById("circle").classList.remove("animategrow");
+}
+
+function UpgradeClick(){
+    score_mult += 1;
 }
