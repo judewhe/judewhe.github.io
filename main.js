d52e732173c5bb37e55208ea525013e8e9e2d44b
judewhe <judewhlr@gmail.com>
2026-07-18 14:14:13 +0100

    adding upgrade requirement and basic button style

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 var score = 0;
-var score_mult = 1
+var score_mult = 1;
+var upgrade_req = 25;
 
 function AnimateButton(){
     document.getElementById("circle").classList.add("animategrow");
     score = score + (1 * score_mult);
     document.getElementById("score").textContent="Score: "+String(score);
+    if (score >= upgrade_req){
+        document.getElementById("upgrade1").classList.remove("upgrade_locked");
+        document.getElementById("upgrade1").classList.add("upgrade_available");
+    }
     setTimeout(RemoveGrowAnimation,100);
 
 }
@@ -13,5 +18,14 @@ function RemoveGrowAnimation(){
 }
 
 function UpgradeClick(){
-    score_mult = score_mult + 1;
+    if (score >= upgrade_req){
+        score_mult = score_mult + 1;
+        score_req = score_req * 2;
+        if (score < upgrade_req){
+            document.getElementById("upgrade1").classList.remove("upgrade_available");
+            document.getElementById("upgrade1").classList.add("upgrade_locked");
+        }
+    }
 }
+
+
