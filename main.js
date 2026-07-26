5d3433a3d772ec7912c4092ec273e4253af23ce2
judewhe <judewhlr@gmail.com>
2026-07-26 16:03:58 +0100

    BUTTON

--- main.js
+++ main.js
@@ -2,15 +2,13 @@ var score = 0;
 var score_mult = 1;
 var upgrade_req = 25;
 
-var upgrade_button = document.getElementById("upgrade1")
-
 function AnimateButton(){
     document.getElementById("circle").classList.add("animategrow");
     score = score + (1 * score_mult);
     document.getElementById("score").textContent="Score: "+String(score);
     if (score >= upgrade_req){
-        upgrade_button.classList.remove("upgrade_locked");
-        upgrade_button.classList.add("upgrade_available");
+        document.getElementById("upgrade1").classList.remove("upgrade_locked");
+        document.getElementById("upgrade1").classList.add("upgrade_available");
     }
     setTimeout(RemoveGrowAnimation,100);
 
@@ -25,11 +23,11 @@ function UpgradeClick(){
         score = score - upgrade_req;
         document.getElementById("score").textContent="Score: "+String(score);
         upgrade_req = upgrade_req * 2;
-        upgrade_button.textContent="Upgrade Click Strength: "+String(upgrade_req)+" Clicks";
+        document.getElementById("upgrade1").textContent="Upgrade Click Strength: "+String(upgrade_req)+" Clicks";
         if (score < upgrade_req){
-            if (upgrade_button.classList.contains("upgrade_locked") == false){
-                upgrade_button.classList.remove("upgrade_available");
-                upgrade_button.classList.add("upgrade_locked");
+            if (document.getElementById("upgrade1").classList.contains("upgrade_locked") == false){
+                document.getElementById("upgrade1").classList.remove("upgrade_available");
+                document.getElementById("upgrade1").classList.add("upgrade_locked");
             } 
         }
     }
