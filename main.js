cb144d28f20e7d1c997e0b409d3b56a717be7b49
judewhe <judewhlr@gmail.com>
2026-07-26 15:37:11 +0100

    button AGAIN the styling wont work ffs

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var score = 0;
 var score_mult = 1;
 var upgrade_req = 25;
 
+var upgrade_button = document.getElementById("upgrade1")
+
 function AnimateButton(){
     document.getElementById("circle").classList.add("animategrow");
     score = score + (1 * score_mult);
     document.getElementById("score").textContent="Score: "+String(score);
     if (score >= upgrade_req){
-        document.getElementById("upgrade1").classList.remove("upgrade_locked");
-        document.getElementById("upgrade1").classList.add("upgrade_available");
+        upgrade_button.classList.remove("upgrade_locked");
+        upgrade_button.classList.add("upgrade_available");
     }
     setTimeout(RemoveGrowAnimation,100);
 
@@ -23,9 +25,12 @@ function UpgradeClick(){
         score = score - upgrade_req;
         document.getElementById("score").textContent="Score: "+String(score);
         upgrade_req = upgrade_req * 2;
+        upgrade_buttom.textContent="Upgrade Click Strength: "+String(upgrade_req)+" Clicks";
         if (score < upgrade_req){
-            document.getElementById("upgrade1").classList.remove("upgrade_available");
-            document.getElementById("upgrade1").classList.add("upgrade_locked");
+            if (upgrade_button.classList.contains("upgrade_locked") == false){
+                upgrade_button.classList.remove("upgrade_available");
+                upgrade_button.classList.add("upgrade_locked");
+            } 
         }
     }
 }
